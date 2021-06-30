496424ed1e0320ba88279a3511a7cfb0399d43bd
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 23:41:37 +0200

    Replace send method with json in customer controller

--- backend/customers/controllers/customers.controllers.ts
+++ backend/customers/controllers/customers.controllers.ts
@@ -4,33 +4,33 @@ import customerService from '../services/customers.service'
 class CustomerController {
     async getAllCustomers(req: express.Request, res: express.Response) {
         const customers = await customerService.getAll(100, 0)
-        res.status(200).send(customers)
+        res.status(200).json(customers)
     }
 
     async getCustomerById(req: express.Request, res: express.Response) {
         const customer = await customerService.getById(req.body.id)
-        res.status(200).send(customer)
+        res.status(200).json(customer)
     }
 
     async createCustomer(req: express.Request, res: express.Response) {
         const userId = await customerService.create(req.body)
-        res.status(201).send({ id: userId })
+        res.status(201).json({ id: userId })
     }
 
 
     async patch(req: express.Request, res: express.Response) {
         await customerService.patchById(req.body.id, req.body)
-        res.status(204).send()
+        res.status(204).json()
     }
 
     async put(req: express.Request, res: express.Response) {
         await customerService.putById(req.body.id, req.body)
-        res.status(204).send()
+        res.status(204).json()
     }
 
     async deleteCustomer(req: express.Request, res: express.Response) {
         await customerService.deleteById(req.body.id)
-        res.status(204).send()
+        res.status(204).json()
     }
 }
 
