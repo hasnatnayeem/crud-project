33e457bd94e8f2622d8077b79d0c9d0afe94f761
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 21:33:54 +0200

    Add placeholder customer routes

---
+++ backend/customers/customers.routes.config.ts
@@ -0,0 +1,40 @@
+import { CommonRoutesConfig } from '../common/common.routes.config'
+import express from 'express'
+
+
+export class CustomerRoutes extends CommonRoutesConfig {
+    constructor(app: express.Application) {
+        super(app, 'CustomerRoutes')
+    }
+
+    configureRoutes(): express.Application {
+        this.app.route(`/customers`)
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send('GET all')
+            })
+            .post((req: express.Request, res: express.Response) => {
+                res.status(200).send(`POST create new`);
+            });
+
+        this.app.route(`/customers/:customerId`)
+            .get((req: express.Request, res: express.Response) => {
+                res.status(200).send(`GET id ${req.params.customerId}`);
+            })
+
+            .put((req: express.Request, res: express.Response) => {
+                res.status(200).send(`PUT id ${req.params.customerId}`);
+            })
+
+            .patch((req: express.Request, res: express.Response) => {
+                res.status(200).send(`PATCH id ${req.params.customerId}`);
+            })
+
+            .delete((req: express.Request, res: express.Response) => {
+                res.status(200).send(`DELETE id ${req.params.customerId}`);
+            });
+
+
+        return this.app
+    }
+
+}
