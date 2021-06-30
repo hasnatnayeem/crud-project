bea4256d39d4e88dd8def0c434571f98390c31fd
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 23:38:05 +0200

    Replace customer routes with controller methods

--- backend/customers/customers.routes.config.ts
+++ backend/customers/customers.routes.config.ts
@@ -1,5 +1,6 @@
-import { CommonRoutesConfig } from '../common/common.routes.config'
 import express from 'express'
+import { CommonRoutesConfig } from '../common/common.routes.config'
+import customerController from './controllers/customers.controllers'
 
 
 export class CustomersRoutes extends CommonRoutesConfig {
@@ -9,30 +10,14 @@ export class CustomersRoutes extends CommonRoutesConfig {
 
     configureRoutes(): express.Application {
         this.app.route(`/customers`)
-            .get((req: express.Request, res: express.Response) => {
-                res.status(200).send('GET all')
-            })
-            .post((req: express.Request, res: express.Response) => {
-                res.status(200).send(`POST create new`);
-            });
+            .get(customerController.getAllCustomers)
+            .post(customerController.createCustomer);
 
         this.app.route(`/customers/:customerId`)
-            .get((req: express.Request, res: express.Response) => {
-                res.status(200).send(`GET id ${req.params.customerId}`);
-            })
-
-            .put((req: express.Request, res: express.Response) => {
-                res.status(200).send(`PUT id ${req.params.customerId}`);
-            })
-
-            .patch((req: express.Request, res: express.Response) => {
-                res.status(200).send(`PATCH id ${req.params.customerId}`);
-            })
-
-            .delete((req: express.Request, res: express.Response) => {
-                res.status(200).send(`DELETE id ${req.params.customerId}`);
-            });
-
+            .get(customerController.getCustomerById)
+            .put(customerController.put)
+            .patch(customerController.patch)
+            .delete(customerController.deleteCustomer);
 
         return this.app
     }
