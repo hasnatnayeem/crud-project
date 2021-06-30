733f4bb3ea1cc57ef780892dbffcb2350c25bae5
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 22:32:37 +0200

    Add customer Dtos

---
+++ backend/customers/dtos/create.customer.dto.ts
@@ -0,0 +1,8 @@
+export interface CraeteCustomerDto {
+    name: string
+    email: string
+    phone: string
+    address: string
+    city: string
+    zipCode: string
+}
---
+++ backend/customers/dtos/put.customer.dto.ts
@@ -0,0 +1,8 @@
+export interface PutCustomerDto {
+    name: string
+    email: string
+    phone: string
+    address: string
+    city: string
+    zipCode: string
+}
