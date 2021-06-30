f751ecac82422d29909db886c3572e72fe9f8032
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 22:21:19 +0200

    Remove debug logger

--- backend/app.ts
+++ backend/app.ts
@@ -3,13 +3,11 @@ import * as http from 'http'
 import cors from 'cors'
 import { CommonRoutesConfig } from './common/common.routes.config'
 import { CustomersRoutes } from './customers/customers.routes.config'
-import debug from 'debug'
 
 const app: express.Application = express()
 const server: http.Server = http.createServer(app)
 const port = 3000;
-const routes: Array<CommonRoutesConfig> = [];
-const debugLog: debug.IDebugger = debug('app')
+const routes: Array<CommonRoutesConfig> = []
 
 
 app.use(express.json()) // Adding body parser
@@ -19,9 +17,5 @@ routes.push(new CustomersRoutes(app))
 
 
 server.listen(port, () => {
-    routes.forEach((route: CommonRoutesConfig) => {
-        debugLog(`Routes configured for ${route.getName()}`);
-    })
-
     console.log(`Server running at http://localhost:${port}`);
 });
