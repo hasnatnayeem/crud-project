1dd4b7314a86ff07942a8f3b0da39ab2b15f436b
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 23:28:11 +0200

    Rename list to getAll in crud interface

--- backend/common/interfaces/crud.interface.ts
+++ backend/common/interfaces/crud.interface.ts
@@ -1,5 +1,5 @@
 export interface CRUD {
-    list: (limit: number, page: number) => Promise<any>
+    getAll: (limit: number, page: number) => Promise<any>
     create: (resource: any) => Promise<any>
     putById: (id: string, resource: any) => Promise<string>
     patchById: (id: string, resource: any) => Promise<string>
