6a7111c5188b5f5b8b570925fee34d1b68b14732
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 23:12:01 +0200

    Change order of method in crud interface

--- backend/common/interfaces/crud.interface.ts
+++ backend/common/interfaces/crud.interface.ts
@@ -1,8 +1,8 @@
 export interface CRUD {
-    list: (limit: number, page: number) => Promise<any>;
-    create: (resource: any) => Promise<any>;
-    putById: (id: string, resource: any) => Promise<string>;
-    readById: (id: string) => Promise<any>;
-    deleteById: (id: string) => Promise<string>;
-    patchById: (id: string, resource: any) => Promise<string>;
+    list: (limit: number, page: number) => Promise<any>
+    create: (resource: any) => Promise<any>
+    putById: (id: string, resource: any) => Promise<string>
+    patchById: (id: string, resource: any) => Promise<string>
+    getById: (id: string) => Promise<any>
+    deleteById: (id: string) => Promise<string>
 }
