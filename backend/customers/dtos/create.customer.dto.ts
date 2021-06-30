75e80ef1c61c1477379bf304710e69f82799e87a
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-06-30 22:36:48 +0200

    Keep optional fields in customer create dto

--- backend/customers/dtos/create.customer.dto.ts
+++ backend/customers/dtos/create.customer.dto.ts
@@ -2,7 +2,7 @@ export interface CraeteCustomerDto {
     name: string
     email: string
     phone: string
-    address: string
-    city: string
-    zipCode: string
+    address?: string
+    city?: string
+    zipCode?: string
 }
