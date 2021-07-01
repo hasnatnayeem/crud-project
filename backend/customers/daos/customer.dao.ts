c12ee4d721689aeae4cf869b9ad5cdf72936542c
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-01 18:31:21 +0200

    Connect mongoose with customer dao

--- backend/customers/daos/customer.dao.ts
+++ backend/customers/daos/customer.dao.ts
@@ -1,28 +1,63 @@
 import { CreateCustomerDto } from '../dtos/create.customer.dto'
 import { PutCustomerDto } from '../dtos/put.customer.dto'
 import { PatchCustomerDto } from '../dtos/patch.customer.dto'
+import mongooseService from '../../common/services/mongoose.service'
 
 class CustomerDao {
+    Schema = mongooseService.getMongoose().Schema
+
+    customerSchema = new this.Schema({
+        _id: String,
+        name: String,
+        email: String,
+        phone: String,
+        address: String,
+        city: String,
+        zipCode: String,
+    }, { id: false })
+
+    Customer = mongooseService.getMongoose().model('Customers', this.customerSchema)
+
     constructor() {}
 
-    async getCustomers(limit = 25, page = 0) {
-        return []
+    async createCustomer(customerData: CreateCustomerDto) {
+        const customerId = mongooseService.generateObjectId()
+        const customer = new this.Customer({
+            _id: customerId,
+            ...customerData
+        })
+
+        await customer.save()
+        return customerId
     }
 
-    async createCustomer(customerData: CreateCustomerDto) {
-        return 1
+    async getCustomers(limit = 25, page = 0) {
+        return this.Customer.find()
+            .limit(limit)
+            .skip(limit * page)
+            .exec()
     }
 
     async getCustomerById(customerId: string) {
-        return {}
+        return this.Customer.findOne({ _id: customerId }).populate('Customer').exec();
     }
 
-    async updateCustomerById(customerId: string, customerData: PutCustomerDto | PatchCustomerDto) {
-        return `updated ${customerId}`
-    }
+    async updateCustomerById(
+        customerId: string, 
+        customerData: PutCustomerDto | PatchCustomerDto
+    ) {
+        const existingCustomer = await this.Customer.findOneAndUpdate(
+            { _id: customerId },
+            { $set: customerData },
+            { new: true }
+        ).exec();
 
+        return existingCustomer;
+    }
+    
+    
     async deleteCustomerById(customerId: string) {
-        return `deleted ${customerId}`
+        return this.Customer.deleteOne({ _id: customerId }).exec();
     }
 
 }
