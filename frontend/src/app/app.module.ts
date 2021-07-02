3d115ad9bd1ecac9cf0c37143a03a1268bf49751
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-02 15:44:45 +0200

    Add mdb ui library

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,12 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CustomersListComponent } from './components/customers-list/customers-list.component';
 import { AddCustomerComponent } from './components/add-customer/add-customer.component';
 import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
+import { MdbFormsModule } from 'mdb-angular-ui-kit/forms';
+import { MdbPopoverModule } from 'mdb-angular-ui-kit/popover';
+import { MdbValidationModule } from 'mdb-angular-ui-kit/validation';
+import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -18,7 +21,11 @@ import { CustomerDetailsComponent } from './components/customer-details/customer
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    MdbFormsModule,
+    MdbPopoverModule,
+    MdbValidationModule,
+    NoopAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
