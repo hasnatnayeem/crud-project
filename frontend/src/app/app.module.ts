cb9a370b5e29d54562e14d90ef3f1398c6d72b62
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-02 14:13:55 +0200

    Add components in frontend

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CustomersListComponent } from './components/customers-list/customers-list.component';
+import { AddCustomerComponent } from './components/add-customer/add-customer.component';
+import { CustomerDetailsComponent } from './components/customer-details/customer-details.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CustomersListComponent,
+    AddCustomerComponent,
+    CustomerDetailsComponent
   ],
   imports: [
     BrowserModule,
