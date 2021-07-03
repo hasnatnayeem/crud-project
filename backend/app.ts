6ab9b81f20125bc420e565d01eb103ce55994445
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-03 19:28:03 +0200

    Add helpet middleware in backend

--- backend/app.ts
+++ backend/app.ts
@@ -6,12 +6,14 @@ if (dotenvResult.error) {
 
 import express from 'express'
 import cors from 'cors'
+import helmet from 'helmet'
 import customersRoutes from './customers/customers.routes'
 import { transformIdProperty } from './common/middlewares/id.middleware';
 
 const app: express.Application = express()
 const router = express.Router()
 
+app.use(helmet()) // security middleware
 app.use(express.json()) // Adding body parser
 app.use(cors()) // Enabling CORS middleware
 app.use(transformIdProperty) // converting 'id' to '_id' for mongoose
