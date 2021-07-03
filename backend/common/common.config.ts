52275e611dafd953f12f658d24b75cb0fd02043e
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-03 20:41:50 +0200

    Rename mongo url env variable

--- backend/common/common.config.ts
+++ backend/common/common.config.ts
@@ -1,2 +1,2 @@
-export const mongoDbConnectionString: string = process.env.MONGO_DB_CONNECTION_STRING || ''
+export const mongoDbConnectionString: string = process.env.MONGO_URL || ''
 export const port = process.env.PORT || 3000
