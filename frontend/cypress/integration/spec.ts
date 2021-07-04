00896767b19a8af6d25a404f96883493f20d99a7
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-04 16:35:27 +0200

    Add e2e tests

--- frontend/cypress/integration/spec.ts
+++ frontend/cypress/integration/spec.ts
@@ -1,7 +1,67 @@
-describe('My First Test', () => {
+import { customers } from './test-data'
+
+describe('Testing CRM application', () => {
+
   it('Visits the initial project page', () => {
     cy.visit('/')
-    cy.contains('Welcome')
-    cy.contains('sandbox app is running!')
+    cy.contains('CRM')
+    cy.contains('New customer')
   })
+
+
+  it('Should open new customer modal', () => {
+    cy.get('.btn-new-customer').first().click()
+    cy.wait(500)
+    cy.contains('Customer details')
+    cy.contains('Name')
+    cy.contains('Email')
+  })
+
+  xit('Should close new customer modal', () => {
+    cy.get('#closeButton').click()
+    cy.wait(500)
+    cy.get('.modal', { timeout: 500 }).should("not.exist");
+  })
+
+
+  it('should create a new customer and show it on the home page', () => {
+    const randomIndex = Math.floor(Math.random() * customers.length);
+    let customer = customers[randomIndex]
+
+    cy.get('.table').find('tr').its('length').then(initialLength => {
+      cy.get('#name').type(customer.name)
+      cy.get('#email').type(customer.email)
+      cy.get('#phone').type(customer.phone)
+      cy.get('#address').type(customer.address)
+      cy.get('#city').type(customer.city)
+      cy.get('#zipCode').type(customer.zipCode)
+  
+      cy.get('#saveButton').click()
+  
+      cy.get('.table').contains(customer.name)
+      cy.get('.table').contains(customer.email)
+      cy.get('.table').find('tr').should('have.length', initialLength + 1)
+    })
+  });
+
+
+  it('should open customer details in modal', () => {
+    cy.get('.btn-customer-details').first().click()
+    cy.wait(500)
+    cy.contains('Customer details')
+    cy.contains('Name')
+    cy.contains('Email')
+
+    cy.get('#closeButton').click()
+    cy.wait(500)
+    cy.get('.modal', { timeout: 500 }).should("not.exist");
+  });
+
+
+  it('should show entered text fro searching', () => {
+    cy.get('#searchInput').type('.com')
+    cy.contains('Showing results')
+  });
+  
 })
+
---
+++ frontend/cypress/integration/test-data.ts
@@ -0,0 +1,58 @@
+export const customers = [
+    {
+        name: 'Maryellen Boyle',
+        email: 'maryellenboyle@calcu.com',
+        phone: '9515002835',
+        address: 'Rock Street',
+        city: 'Eagleville',
+        zipCode: '2799'
+    },
+    {
+        name: 'Maude Knight',
+        email: 'maudeknight@calcu.com',
+        phone: '8475052569',
+        address: 'Brightwater Av',
+        city: 'Tilleda',
+        zipCode: '2050'
+    },
+    {
+        name: 'Sharpe Howe',
+        email: 'sharpehowe@calcu.com',
+        phone: '8485693993',
+        address: 'Anna Court',
+        city: 'Wawona',
+        zipCode: '4491'
+    },
+    {
+        name: 'Holden Roy',
+        email: 'holdenroy@calcu.com',
+        phone: '9614983066',
+        address: 'Ryder Street',
+        city: 'Villarreal',
+        zipCode: '7581'
+    },
+    {
+        name: 'Carla Mitchell',
+        email: 'carlamitchell@calcu.com',
+        phone: '8844072690',
+        address: 'Bradford Stree',
+        city: 'Elliott',
+        zipCode: '3120'
+    },
+    {
+        name: 'Nadine Wilson',
+        email: 'nadinewilson@calcu.com',
+        phone: '8444923365',
+        address: 'Linwood Street',
+        city: 'Trucksville',
+        zipCode: '7634'
+    },
+    {
+        name: 'Wendi Church',
+        email: 'wendichurch@calcu.com',
+        phone: '8585332260',
+        address: 'Crystal Street',
+        city: 'Vivian',
+        zipCode: '6363'
+    }
+]
