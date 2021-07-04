9ef45bbc4f97f9ab8c1547988101588808492480
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-04 13:07:08 +0200

    Add snippet to customer details test

--- frontend/src/app/components/customer-details/customer-details.component.spec.ts
+++ frontend/src/app/components/customer-details/customer-details.component.spec.ts
@@ -1,27 +1,57 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { OverlayContainer } from '@angular/cdk/overlay';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { ComponentFixture, fakeAsync, inject, TestBed } from '@angular/core/testing';
 import { MdbModalModule, MdbModalRef, MdbModalService} from 'mdb-angular-ui-kit/modal';
 
 import { CustomerDetailsComponent } from './customer-details.component';
 
+@NgModule({
+  declarations: [CustomerDetailsComponent],
+  entryComponents: [CustomerDetailsComponent],
+  imports: [HttpClientModule],
+  providers: [MdbModalRef]
+})
+class TestModalModule {}
+
+
 describe('CustomerDetailsComponent', () => {
-  let component: CustomerDetailsComponent;
+  let modal: MdbModalService;
+  let overlayContainer: OverlayContainer;
+  let overlayContainerElement: HTMLElement;
+  let modalRef: MdbModalRef<CustomerDetailsComponent>
   let fixture: ComponentFixture<CustomerDetailsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ CustomerDetailsComponent ],
-      imports: [MdbModalModule]
-    })
-    .compileComponents();
+  beforeEach(fakeAsync(() => {
+    const module = TestBed.configureTestingModule({
+      imports: [MdbModalModule, TestModalModule],
+    });
+
+    TestBed.compileComponents();
+    fixture = module.createComponent(CustomerDetailsComponent);
+  }));
+  
+
+  beforeEach(inject(
+    [MdbModalService, OverlayContainer, MdbModalRef , CustomerDetailsComponent],
+    (mdbModal: MdbModalService, oc: OverlayContainer, mr: MdbModalRef<CustomerDetailsComponent>) => {
+      modal = mdbModal;
+      overlayContainer = oc;
+      overlayContainerElement = oc.getContainerElement();
+      modalRef = mr
+    }
+  ));
+
+  afterEach(() => {
+    overlayContainer.ngOnDestroy();
   });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(CustomerDetailsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+  it('should open a modal with a  specified component', () => {
+    modal.open(CustomerDetailsComponent);
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+    expect(overlayContainerElement.textContent).toContain('Customer details');
+
+    const modalContainer = overlayContainerElement.querySelector('mdb-modal-container');
+    expect(modalContainer).not.toBe(null);
   });
 });
