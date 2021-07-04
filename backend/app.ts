273518f16e939783aed74a1215d542d36ef762df
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-04 21:41:06 +0200

    Add swagger

--- backend/app.ts
+++ backend/app.ts
@@ -1,12 +1,14 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+import * as swaggerDocument from './swagger.json'
 import cors from 'cors'
 import helmet from 'helmet'
 import customersRoutes from './customers/customers.routes'
 import { transformIdProperty } from './common/middlewares/id.middleware';
+
 
 const app: express.Application = express()
 const router = express.Router()
@@ -18,8 +20,9 @@ app.use(transformIdProperty) // converting 'id' to '_id' for mongoose
 
 
 router.use('/customers', customersRoutes)
-
 app.use('/v1', router)
+app.use('/v1/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
 
 
 export default app
