2df5c61f6c9c03fe7c02a382c37b6ce39666414d
Md Abul Hasnat Nayeem <md-abul-hasnat.nayeem@obcc.de>
2021-07-05 03:20:14 +0200

    Add event que comments

--- frontend/src/app/services/event-queue/event-queue.service.ts
+++ frontend/src/app/services/event-queue/event-queue.service.ts
@@ -4,10 +4,12 @@ import { filter } from 'rxjs/operators';
 import { AppEventType } from './app-event-type.enum';
 import { AppEvent } from './app-event.class';
 
+// Used as a event broker throughout the application
+// Components can communicate with each other even if they are not in parent child relationship
+
 @Injectable({
   providedIn: 'root'
 })
-
 export class EventQueueService {
   private eventBrocker = new Subject<AppEvent<any>>();
 
